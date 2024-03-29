a4deb8a5064ea1f4ead5929e105cca32ae5419fb
Henry Virieux <105490926+hr-virieux@users.noreply.github.com>
2024-03-29 13:35:01 +1100

    Create htmlRoutes.js
    HTML routes test

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+// Import packages
+const path = require('path');
+const router = require('express').Router();
+
+// Route to serve the notes.html file
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+// Catch-all route to serve the index.html file
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = router;
