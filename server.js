ceafe74a34f3581dfaba02b1dfc2d069007a5c25
Henry Virieux <105490926+hr-virieux@users.noreply.github.com>
2024-03-31 20:39:08 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
-// Node.js modules
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-// Express app
 const app = express();
-const PORT = process.env.PORT || 3001; // Define the port
+const PORT = process.env.PORT || 3001;
 
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// Routes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-// Start the server
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
