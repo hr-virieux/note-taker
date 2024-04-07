ece68de912a8ef0ebd84f98f1d6d99ab9a8a8653
Henry Virieux <105490926+hr-virieux@users.noreply.github.com>
2024-04-07 16:29:52 +1000

    Update server.js
    Signed-off-by: Henry Virieux <105490926+hr-virieux@users.noreply.github.com>

--- server.js
+++ server.js
@@ -5,10 +5,16 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+// Use apiRoutes and htmlRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
